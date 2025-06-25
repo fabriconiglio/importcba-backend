eab4c1411694d22f3a3634e3ade3bb1618834fd8
fabrizzio <fabrizzio.coniglio@minimalart.co>
2025-06-25 20:54:36 -0300

    Add brand logo and height configuration to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -58,6 +58,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->brandLogo(asset('images/logo.png'))
+            ->brandLogoHeight('5rem');
     }
 }
