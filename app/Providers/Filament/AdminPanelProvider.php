0c9fadea70cd96e4907679f09a7ccc443f24f2d3
fabrizzio <fabrizzio.coniglio@minimalart.co>
2025-06-25 20:33:31 -0300

    Update AdminPanelProvider color scheme with new primary, danger, warning, info, and success colors

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,11 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#2392D2',   // Celeste
+                'danger'  => '#EB4B2C',   // Rojo
+                'warning' => '#FEE050',   // Amarillo
+                'info'    => '#2C4B8E',   // Azul
+                'success' => '#02955F',   // Verde
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
