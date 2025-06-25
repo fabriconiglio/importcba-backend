0191c296c53440bc0191b22fd3a383b4d2bab7d5
fabrizzio <fabrizzio.coniglio@minimalart.co>
2025-06-25 20:26:54 -0300

    Replace default widgets in AdminPanelProvider with new statistics widgets: Ingresos, PedidosPorMes, and TotalClientes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,8 +37,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                \App\Filament\Widgets\Ingresos::class,
+                \App\Filament\Widgets\PedidosPorMes::class,
+                \App\Filament\Widgets\TotalClientes::class,
             ])
             ->middleware([
                 EncryptCookies::class,
