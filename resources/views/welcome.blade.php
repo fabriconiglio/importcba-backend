2e595c448de9ce99e7bcc6cfd6b0c309f1eec4b7
fabrizzio <fabrizzio.coniglio@minimalart.co>
2025-08-27 00:08:44 -0300

    Agregar favicon y logo en la vista de bienvenida. Se incluye el enlace al favicon en formato .ico y un logo en formato .png para mejorar la presentación visual de la aplicación.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,10 @@
 
         <title>Laravel</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
