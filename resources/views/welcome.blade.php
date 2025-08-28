5430e474b27488d48888bf312b8b4886fc25407b
fabrizzio <fabrizzio.coniglio@minimalart.co>
2025-08-27 21:58:03 -0300

    Corregir el enlace del logo en la vista de bienvenida, eliminando un espacio innecesario en la etiqueta `<link>` para asegurar una correcta carga del icono en formato .png.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Favicon -->
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png')}}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
